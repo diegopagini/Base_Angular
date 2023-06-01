7ddb507b29569b674efa596210682268dbdd13b4
Diego Pagini <diego.pagini@neoris.com>
2023-06-01 08:27:59 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule)],
+}).catch((err) => console.error(err));
